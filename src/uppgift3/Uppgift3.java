a964aacb67b47b79dc30e1e2e14f5f29de54e418
Nasim Rafieiyan <nasim.rafieiyan@yh.nackademin.se>
2017-10-26 20:23:57 +0200

    inlämningsuppgift 3 (OOP 2017)

---
+++ src/uppgift3/Uppgift3.java
@@ -0,0 +1,19 @@
+package uppgift3;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class Uppgift3  extends JFrame {
+    
+    //constructor
+    public Uppgift3(){
+    
+    
+    }
+    
+    public static void main(String[] args) {
+
+    }
+
+}
