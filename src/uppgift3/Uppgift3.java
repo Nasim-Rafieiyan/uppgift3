de89a8babde6ba00b74925eb4ac5a80a684bfdaf
Nasim Rafieiyan <nasim.rafieiyan@yh.nackademin.se>
2017-10-26 20:38:51 +0200

    inlämningsuppgift 3 (OOP 2017)

--- src/uppgift3/Uppgift3.java
+++ src/uppgift3/Uppgift3.java
@@ -6,6 +6,10 @@ import java.awt.event.*;
 
 public class Uppgift3  extends JFrame {
     
+    //variables
+    private JButton b1, b2, b3, b4, b5, b6, b7, b8;
+    private JButton b9, b10, b11, b12, b13, b14, b15, bEmpty;
+    
     //constructor
     public Uppgift3(){
     
